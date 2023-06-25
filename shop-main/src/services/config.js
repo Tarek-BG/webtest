527c091be38c7d8b1fb98053519cf3cce286c430
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2023-06-25 18:15:49 +0000

    Update config.js

--- shop-main/src/services/config.js
+++ shop-main/src/services/config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '1:',
-  measurementId: ''
+  apiKey: "AIzaSyCEQ8hjamQeafiIl3o-Bjd2PK_BG-tar3g",
+  authDomain: "start-godot2020.firebaseapp.com",
+  databaseURL: "https://start-godot2020.firebaseio.com",
+  projectId: "start-godot2020",
+  storageBucket: "start-godot2020.appspot.com",
+  messagingSenderId: "601265281914",
+  appId: "1:601265281914:web:c422530a00790c17e6d4c3",
+  measurementId: "G-2LR5KD08S5"
 };
 
 export default firebaseConfig;
