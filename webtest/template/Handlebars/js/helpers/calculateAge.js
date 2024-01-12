4a2e34e9d9034840886072096ff237f3ac10edf0
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2024-01-12 19:05:47 +0000

    hb

--- webtest/template/Handlebars/js/helpers/calculateAge.js
+++ webtest/template/Handlebars/js/helpers/calculateAge.js
@@ -2,8 +2,8 @@ module.exports = function(birthYear) {
   var age = new Date().getFullYear() - birthYear;
 
   if (age > 0) {
-    return age + " years old";
+    return age + " years old.";
   } else {
-    return "Less than a year old";
+    return "Less than a year old.";
   }
 };
