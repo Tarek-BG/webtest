d49a5ac2e0eea461f76472bd6787e37a22729a73
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2024-08-13 19:22:36 +0100

    xl

--- 2xlsxLocal/xlsConvertjson/excel.js
+++ 2xlsxLocal/xlsConvertjson/excel.js
@@ -19,12 +19,13 @@ document.getElementById('button').addEventListener("click", () => {
         fileReader.onload = (event)=>{
          let data = event.target.result;
          let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
+         console.log(jsondata);
          workbook.SheetNames.forEach(sheet => {
               let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
               console.log(rowObject);
               document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
+        
+            });
         }
     }
 });
