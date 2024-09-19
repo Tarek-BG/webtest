7b1199a41254366c0bab1db78a217543308b6432
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2024-09-19 12:07:18 +0100

    j2h

--- webtest/template/json2html/apps9.js
+++ webtest/template/json2html/apps9.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.btn');
 const btn1 = document.querySelector('.btn1');
 const output = document.querySelector('.output');
-const url = 'pets-data.json';
+const url = "pets-data.json";
 btn.onclick = ()=>{
     output.innerHTML = 'Connecting......';
     getdata();
@@ -12,6 +12,7 @@ function getData(){
     fetch(url)
     .then(rep => rep.json())
     .then(data =>{
+        console.log(data);
         output(data.species)
     })
 }
