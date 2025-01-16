ae1a37131215d8df8491d64bb2eb2272804638ea
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2025-01-16 19:03:12 +0100

    js

--- webtest/jqx/script.js
+++ webtest/jqx/script.js
@@ -1,6 +1,6 @@
-console.log("Hello, tarek!");
+console.log("Hello, tarek !");
 
 fetch("https://tarek-bg.github.io/web/webtest/template/Handlebars/data/pets-data1.json").then
 (function(res){
-    console.log(res);
+    console.log(res.json());
 });
