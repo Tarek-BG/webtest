9e85b79adb66d9b5f8f9323446ea53df68f26d0e
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2025-01-16 20:18:10 +0100

    js

--- webtest/jqx/script.js
+++ webtest/jqx/script.js
@@ -3,8 +3,8 @@ console.log("Hello, tarek ! freejqGrid_fetch_script ");
 fetch("https://tarek-bg.github.io/web/webtest/jqx/fetch.json").then
 (function(res){
     return res.json();    
-}).then(function(data){
-    console.log(data);
+}).then(function(Gdata){
+    console.log(Gdata);
 
 
 });
