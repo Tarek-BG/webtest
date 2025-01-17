3a7188e9b5002de66d9d0c79aba674ce017bf467
Tarek-BG <61729495+Tarek-BG@users.noreply.github.com>
2025-01-17 15:04:42 +0100

    js

--- webtest/jqx/script.js
+++ webtest/jqx/script.js
@@ -4,7 +4,7 @@ fetch("https://tarek-bg.github.io/web/webtest/jqx/fetches.json").then
 (function(res){
     return res.json();    
 }).then(function(data){
-    console.log("data from script  ="+data);
-
+    console.log("data from script");
+    console.log(data);
 
 });
